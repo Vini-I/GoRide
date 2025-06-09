23f53d14c5db33787ff0a22329112f694ffc2460
Brwni(Vini) <mavasquezba@est.utn.ac.cr>
2025-06-09 16:59:05 -0600

    Correcion de la clase User
    Se cambia la clase User por un error con sus metodos getter, setter, una verificacion para cambiar el numero telefonico y su metodo toString

--- src/Users/user.java
+++ src/Users/user.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Users;
 
-/**
- *
- * @author autoa
- */
+
+
 public class user {
+    private String name;
+    private int id;
+    private String phone;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPhone(String phone) {
+        if (verifyPhone(phone)) {
+            this.phone = phone;
+        }
+    }
     
+    public static boolean verifyPhone(String phone) {
+        String regex = "^[245678]\\\\d{7}$";
+        return phone.matches(regex);
+    }
+
+    @Override
+    public String toString() {
+        return "\nUser:\nName: " + name + "\nId: " + id + "\nPhone: " + phone + "\nEmail:" + email;
+    }
 }
