f6094307bb5fee707fbca3bb64c9efceca6b3948
Marco Vinicio Vasquez Barrantes <mavasquezba@est.utn.ac.cr>
2025-06-16 15:34:06 -0600

    Creacion de listas
    Se crean las listas para cada clase y se aplica la implementacion de la interfaz Lists que se crearon antes

--- src/Stations/StationList.java
+++ src/Stations/StationList.java
@@ -4,10 +4,33 @@
  */
 package Stations;
 
+import GoRide.Lists;
+
 /**
  *
  * @author autoa
  */
-public class StationList {
+public class StationList implements Lists<Station> {
+    private Station[] list;
+
+    @Override
+    public boolean add(Station t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean delete(Station t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void sort() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Station search(Object id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
