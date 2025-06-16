84cd9445e21b46e388cfd6608da205bfc326c1e4
Marco Vinicio Vasquez Barrantes <mavasquezba@est.utn.ac.cr>
2025-06-16 16:47:53 -0600

    Arreglo en las clases Bicycle y funcionalidad de la BicycleList

--- src/GoRide/Main.java
+++ src/GoRide/Main.java
@@ -1,11 +1,18 @@
 package GoRide;
 
-
+import Bicycles.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        Bicycle b1 = new Bicycle(1, "Specialized");
+        Bicycle b2 = new ElectricBicycle(2, "Energy");
+        Bicycle b3 = new GasBicycle(3, "Yamaha");
         
+        BicycleList bicycleList = new BicycleList();
+        System.out.println(bicycleList.add(b1));
+        System.out.println(bicycleList.add(b3));
+        System.out.println(bicycleList.add(b2));
     }
     
 }
