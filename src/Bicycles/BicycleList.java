25986849356f7c88aff7bc008364ff93776086a1
Marco Vinicio Vasquez Barrantes <mavasquezba@est.utn.ac.cr>
2025-06-16 16:06:48 -0600

    Ajuste en la lista de bicicletas
    Se agrego el codigo de funcionalidad a la lista bicicletas

--- src/Bicycles/BicycleList.java
+++ src/Bicycles/BicycleList.java
@@ -15,22 +15,52 @@ public class BicycleList implements Lists<Bicycle> {
 
     @Override
     public boolean add(Bicycle t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] == null) {
+                list[i] = t;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean delete(Bicycle t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] == t) {
+                list[i] = null;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public void sort() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int max = list.length;
+        for (int i = 0; i < max; i++) {
+            for (int j = 0; j < max-1; j++) {
+                if (list[j] != null && list[j+1] != null) {
+                    if (list[j].getId() > list[j+1].getId()) {
+                        Bicycle temp = list[j];
+                        list[j] = list[j+1];
+                        list[j+1] = temp;
+                    }
+                }
+            }
+        }   
     }
 
     @Override
     public Bicycle search(Object id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int idt = Integer.parseInt(id.toString());
+        int max = list.length;
+        for (int i = 0; i < max; i++) {
+            if (list[i].getId() == idt) {
+                return list[i];
+            }
+        }
+        return null;
     }
     
 }
